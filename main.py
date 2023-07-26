c2da628e16c5b3f014d776bbc1918e5658bc7709
Kazemidori <mail@nekokawa.net>
2023-07-26 13:28:31 +0900

    画像選択部分をもっと綺麗にした

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import random
 import time
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,21 +20,27 @@ data = {
     'limit': 100
 }
 
+
 def imgChoice():
     r = requests.post(uploadurl, headers=header, json=data)
     result = r.json()
-    rnd = random.randint(0, 58)
+    rnd = random.randint(0, len(result))
+    print(len(result))
     imgId = result[rnd]['id']
     return imgId
 
 
 while True:
-    notedata = {
-        'i': token,
-        'mediaIds': [imgChoice()]
-    }
-    r = requests.post(noteurl, headers=header, json=notedata)
-    result = r.json()
-    print(f"posted: https://{instance}/notes/{result['createdNote']['id']}")
-    time.sleep(60 * 60)
-    continue
+    if datetime.now().strftime("%M%S") == "0000":
+        notedata = {
+            'i': token,
+            'mediaIds': [imgChoice()]
+        }
+        r = requests.post(noteurl, headers=header, json=notedata)
+        result = r.json()
+        print(f"posted: https://{instance}/notes/{result['createdNote']['id']}")
+        time.sleep(60 * 60)
+        continue
+    else:
+        time.sleep(60)
+        continue
